d803558733730508c02db8cdee8f407e1089a332
Mike Boyle <mboyle1357@gmail.com>
2024-08-10 23:25:17 +0800

    Fix User model and update for deployment

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
 from . import db
 from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):  # Inherit from UserMixin
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    # Adding a new column
     last_login = db.Column(db.DateTime, nullable=True)
+
 
 
 class HighScore(db.Model):
