dc7cfbee0e3b2156da3a1eff3237ab1959fd27a5
Mike Boyle <mboyle1357@gmail.com>
2024-08-16 20:20:03 +1000

    updated time to 60 secs

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    TIMER_VALUE = int(os.environ.get('TIMER_VALUE', 10))
+    TIMER_VALUE = int(os.environ.get('TIMER_VALUE', 60))
